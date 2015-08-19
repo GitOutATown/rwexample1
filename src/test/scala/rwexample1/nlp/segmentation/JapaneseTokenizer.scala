e74dce83d4b77d8e8cff6c942619fcd5cc618c31
GitOutATown <aqumegeemail@gmail.com>
2015-08-19 13:35:47 -0700

    fixed indents

--- src/test/scala/rwexample1/nlp/segmentation/JapaneseTokenizer.scala
+++ src/test/scala/rwexample1/nlp/segmentation/JapaneseTokenizer.scala
@@ -1,4 +1,4 @@
-package com.qyoom.nlp.segmentation
+package rwexample1.nlp.segmentation
 
 import play.api.mvc._
 import play.api.libs.json._
@@ -12,44 +12,39 @@ import scala.collection.JavaConversions._
 
 object JapaneseTokenizer extends Controller {
   
-	// Simple string tokenize
-	def tokenizeString = Action { implicit request =>
-	  	request.body.asJson.map { json =>
-	  	  	val sourceText = (json \ "srcTxt").validate[String] // @TODO: Needs rigor!
-	  	  	val tokenizer = AbstractTokenizer.builder().build()
-	  	  	val tokens = tokenizer.tokenize(sourceText.get)
-	  	  	
-	  	  	val output = for {
-	  	  		token <- tokens
-	  	  	} yield Json.obj("token" -> token.getSurfaceForm(), "features" -> token.getAllFeatures())
-	
-  			Ok(Json.toJson(output))
-	    
-	    }.getOrElse {
-			BadRequest("Expecting Json data")
-		}
-	}
-	
-	// File upload and tokenize
-	def tokenizeFile = Action(parse.multipartFormData) { request =>
-		request.body.file("file_ja").map { file =>
-			val srcFile = file.ref.file // @TODO: Is this a disk write? If so, is it necessary? If so, does it need to be managed (i.e. deleted)?
-			val lines = scala.io.Source.fromFile(srcFile)
-			val lineList = lines.getLines.toList
-			
-			val tokenizer = AbstractTokenizer.builder().build()
-			val tokLines = lineList map { line =>
-			   	val tokens = tokenizer.tokenize(line)
-			   	val output = for {
-			   		token <- tokens
-			   	} yield Json.obj("token" -> token.getSurfaceForm(), "features" -> token.getAllFeatures())
-			   	output
-			}
-			
-			Ok(Json.toJson(tokLines))
-		}.getOrElse {
-			BadRequest("Missing file")
-		}
-	}
+    // Simple string tokenize
+    def tokenizeString = Action { implicit request =>
+          request.body.asJson.map { json =>
+              val sourceText = (json \ "srcTxt").validate[String]
+              val tokenizer = AbstractTokenizer.builder().build()
+              val tokens = tokenizer.tokenize(sourceText.get)
+              val output = for {
+                  token <- tokens
+              } yield Json.obj("token" -> token.getSurfaceForm(), "features" -> token.getAllFeatures())
+              Ok(Json.toJson(output))
+        }.getOrElse {
+            BadRequest("Expecting Json data")
+        }
+    }
+    
+    // File upload and tokenize
+    def tokenizeFile = Action(parse.multipartFormData) { request =>
+        request.body.file("file_ja").map { file =>
+            val srcFile = file.ref.file
+            val lines = scala.io.Source.fromFile(srcFile)
+            val lineList = lines.getLines.toList
+            val tokenizer = AbstractTokenizer.builder().build()
+            val tokLines = lineList map { line =>
+                val tokens = tokenizer.tokenize(line)
+                val output = for {
+                    token <- tokens
+                } yield Json.obj("token" -> token.getSurfaceForm(), "features" -> token.getAllFeatures())
+                output
+            }
+            Ok(Json.toJson(tokLines))
+        }.getOrElse {
+            BadRequest("Missing file")
+        }
+    }
 
 }
